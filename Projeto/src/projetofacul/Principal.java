84e55adfc75fb5b294388b238a345f0fe0a5af90
cauematheus <cauematheus_23@hotmail.com>
2023-09-20 14:16:35 -0300

    inicial
    Adicionando consultar dados, deletar dados, atualizar e o loop para gerenciar o que futuramente sera um Menuzinho

---
+++ Projeto/src/projetofacul/Principal.java
@@ -0,0 +1,147 @@
+package projetofacul;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class Principal 
+{
+	public static void main(String[] args) {
+// 0 - adicionar o arquivo .jar	da biblioteca SQLite JDBC
+		// 1 - identificar qual que é o caminho do arquivo de banco de dados
+			String jdbcUrl = "jdbc:sqlite:/C:\\Users\\cauem\\eclipse-workspace\\Projeto\\BDHOTEL.db";
+			
+	try {
+		// 2 - conectar com o banco de dados
+		Connection connection = DriverManager.getConnection(jdbcUrl);
+		while (true) {
+			Scanner scanner = new Scanner(System.in); // criar um objeto da classe Scanner
+			System.out.println("[1] Consultar Dados\n[2] Inserir Dados\n[3] Deletar Dados\n[4] Finalizar programa" );
+			int escolha = scanner.nextInt();
+			if (escolha == 1) {
+				consultarDados(connection);
+			} else if (escolha == 2) {
+				cadastrarDados(connection);
+			} else if (escolha == 3) {
+				deletarDados(connection);
+			} else if (escolha == 4) {
+				break ;
+			} else {
+				System.out.println("DIGITE UM VALOR VALIDO");
+			}
+			
+		}
+	}  catch (SQLException e) {
+		System.out.println("ERRO" + e);
+		e.printStackTrace();
+		}
+}
+	
+	public static void consultarDados(Connection connection) {
+	try {	
+		// 3 - definir a query SQL
+		String sql = "SELECT * from clientes";
+		// 4 - abrir a conexão com o banco de dados 
+		Statement statement = connection.createStatement();
+		// 5 - executar a query
+		ResultSet result = statement.executeQuery(sql);
+		// 6 - acessar e exibir o resultado
+		System.out.println("---------------------------");
+		System.out.println("| ID | NOME | Email | Telefone |");
+		System.out.println("---------------------------");
+		while(result.next()) 
+	{ String idclientes = result.getString("idclientes");
+			String nomecliente = result.getString("nomecliente");
+			String emailcliente = result.getString("emailcliente");
+			String telcliente = result.getString("telcliente");
+
+
+		    System.out.printf("| %-4d | %-10s | %-15s | %-10s |\n", idclientes, nomecliente, emailcliente, telcliente);
+	}
+		System.out.println("----------------------");
+
+		}	catch (SQLException e) 
+			{	System.out.println("ERRO" + e);
+			e.printStackTrace();
+			}
+	
+	}
+	
+	public static void cadastrarDados(Connection connection) {
+		Scanner scanner = new Scanner(System.in); // criar um objeto da classe Scanner
+		System.out.println("Informe o ID(inteiro e unico) da pessoa que deseja inserir no bd:  ");
+		int idclientes = scanner.nextInt();
+		scanner.nextLine();
+		System.out.println("Informe o nome do cliente que deseja inserir no bd:  ");
+		String nomecliente = scanner.nextLine();
+		System.out.println("Informe o email do cliente que deseja inserir no bd:  ");
+		String emailcliente = scanner.nextLine();
+		System.out.println("Informe o Telefone do cliente que deseja inserir no bd:  ");
+		String telcliente = scanner.nextLine();
+		String sql = "INSERT INTO clientes (idclientes,nomecliente,emailcliente,telcliente) VALUES (?, ?, ?, ?)";
+		try {
+			PreparedStatement preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setInt(1,idclientes);
+			preparedStatement.setString(2, nomecliente);
+			preparedStatement.setString(3, emailcliente);
+			preparedStatement.setString(4, telcliente);
+			int rowsAffected = preparedStatement.executeUpdate();
+			if (rowsAffected > 0) {
+				System.out.println("Registro inserido com sucesso!");
+			} else {
+				System.out.println("Falha ao inserir o registro");
+			}
+		}catch (SQLException e) {
+			System.out.println("Erro ao inserir dados: " + e.getMessage());
+		}
+}
+	public static void atualizarDados(Connection connection) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Digite o ID (inteiro) da pessoa que o nome vai ser atualizado: ");
+		int id = scanner.nextInt();
+		scanner.nextLine();
+		System.out.println("Digite o novo nome da pessoa: ");
+		String novonome = scanner.nextLine();
+		String sql = "UPDATE pessoas SET nome = ? WHERE id = ?";
+		try {
+			PreparedStatement preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setString(1, novonome);
+			preparedStatement.setInt(2, id);
+			int rowsAffected = preparedStatement.executeUpdate();
+			if(rowsAffected > 0) {
+				System.out.println("Registro atualizado com sucesso");
+			} else {
+				System.out.println("Nenhum registro foi atualizado. Verifique o ID informado.");
+			}
+		}	catch(SQLException e) {
+			System.out.println("Erro ao atualizar registro: " + e.getMessage());
+					
+		}
+		}
+	public static void deletarDados(Connection connection) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Digite o ID (inteiro) da pessoa que deseja deletar do banco: ");
+		int idcliente = scanner.nextInt();
+		String sql = "DELETE FROM clientes WHERE idclientes = ?";
+		try {
+			PreparedStatement preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setInt(1, idcliente);
+			int rowsAffected = preparedStatement.executeUpdate();
+			if(rowsAffected > 0) {
+				System.out.println("Registro deletado com sucesso");
+			} else {
+				System.out.println("Nenhum registro encontrado. Verifique o ID informado.");
+			}
+		}	catch(SQLException e) {
+			System.out.println("Erro ao deletar registro: " + e.getMessage());
+					
+		}
+		}
+	
+	
+		
+}
